276b6146015a2929bd27dd32bf5b07b5958c7f1d
Lê Duy Vũ <djanh123456@gmail.com>
2026-06-18 15:25:45 +0700

    step 2: thiet lap quy uoc du an, custom responses va ma loi

---
+++ backend/api/services/respCode.js
@@ -0,0 +1,6 @@
+module.exports = {
+    SUCCESS: 200,
+    BAD_REQUEST: 400,
+    SERVER_ERROR: 500,
+    // Sau này có lỗi nghiệp vụ gì riêng (như Sai mật khẩu, Thiếu số dư...) ta sẽ khai báo thêm ở đây.
+};
